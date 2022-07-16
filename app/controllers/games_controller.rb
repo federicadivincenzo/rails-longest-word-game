ab9b03e0ccbc05cba319c50d537590518c2d95cd
Federica Di Vincenzo <federicadivincenzo5@gmail.com>
2022-07-16 10:16:04 +0200

    created score logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @alph = ('A'..'Z').to_a
@@ -5,9 +7,17 @@ class GamesController < ApplicationController
   end
 
   def score
-    @word = params[:word]
-    @word.chars.each do |letter|
-      return letter;
+    @word = params[:word].chars
+    letters = new()
+    letters_clean = letters.map { |el| el.downcase }
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{@word.join}")
+    data = JSON.parse(response.read)
+    letters_clean.each { |el| @word.delete_at(@word.index(el)) if @word.index(el) }
+raise
+
+    if data['found']
+      @attempt = @word
     end
   end
+
 end
